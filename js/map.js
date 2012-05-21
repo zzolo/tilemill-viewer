90fb5ce2da4c2da36f1a6b3e540c4a044ef708ae
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-05-21 11:11:23 -0500

    Handling layers better.

--- js/map.js
+++ js/map.js
@@ -4,27 +4,47 @@
 $(document).ready(function() {
   var map = new L.Map('map');
   var layer;
-  var base_url = 'http://localhost:20008/tile';
+  var baseUrl = 'http://localhost:20008/tile';
+  var baselayers = {};
+  var overlays = {};
+  var layerControl = {};
 
   // Set default and load default layer
   $('input.update-map').val('geography-class');
-  var base = $('input.update-map').val();
-  var url = base_url + '/' + base + '/{z}/{x}/{y}.png';
   
-	layer = new L.TileLayer(url, { maxZoom: 15, minZoom: 2, scheme: 'xyz' });
-	map.setView(new L.LatLng(46.1494,-94.4495), 7).addLayer(layer);
+  // Add basic layers and control
+  layerControl = new L.Control.Layers(baselayers, overlays);
+  map.addControl(layerControl);
+	
+	// Function load map
+	function loadMap(project) {
+    var base = $('input.update-map').val();
+    var url = baseUrl + '/' + base + '/{z}/{x}/{y}.png';
+    
+    // Add new layer
+    layer = new L.TileLayer(url, { maxZoom: 15, minZoom: 2, scheme: 'xyz' });
+    map.setView(new L.LatLng(0, 0), 3).addLayer(layer);
+    
+    // Add layer to overlays and re-add control
+    layerControl.addOverlay(layer, base);
+    
+    // TileMill does not produce a tilejson feed or jsonp version. :(
+    // Use tilejson to load it up
+    /*
+    wax.tilejson(base_url + '/' + base + '.jsonp',
+      function(tilejson) {
+      console.log(tilejson);
+      }
+    );
+    */
+	};
+	
+  // Load the default map
+  loadMap();
 
   // Allow for input of other maps
   $('.map-form form').on('submit', function(e) {
     e.preventDefault();
-    if (layer) {
-      map.removeLayer(layer);
-    }
-    var base = $('input.update-map').val();
-    var url = base_url + '/' + base + '/{z}/{x}/{y}.png';
-    
-		layer = new L.TileLayer(url, { maxZoom: 15, minZoom: 2, scheme: 'xyz' });
-		map.setView(new L.LatLng(46.1494,-94.4495), 7).addLayer(layer);
-    
+    loadMap();
   });
 });
