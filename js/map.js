dc75de0fb1256f6ac1a63a9e47f0082eb91c0777
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-05-21 11:22:38 -0500

    Adding base maps.

--- js/map.js
+++ js/map.js
@@ -12,7 +12,28 @@ $(document).ready(function() {
   // Set default and load default layer
   $('input.update-map').val('geography-class');
   
-  // Add basic layers and control
+  // OSM base layer
+  var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    minZoom: 0, maxZoom: 22, attribution: 'Map data © OpenStreetMap contributors'
+  });
+  baselayers['Default OSM'] = osm;
+	map.addLayer(osm);
+	// Mapbox streets
+	mapboxStreets = new L.TileLayer('http://{s}.tiles.mapbox.com/v3/mapbox.mapbox-streets/{z}/{x}/{y}.png', {
+    attribution: 'Map imagery from <a href="http://mapbox.com">Mapbox</a>'
+  });
+  baselayers['Mapbox Streets'] = mapboxStreets;
+	map.addLayer(osm);
+	// Mapquest
+  var mapquest = new L.TileLayer('http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+    maxZoom: 18, subdomains: ['otile1','otile2','otile3','otile4'],
+    attribution: 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.'
+  });
+  baselayers['Mapquest'] = mapquest;
+	map.addLayer(osm);
+
+	
+	// Add control
   layerControl = new L.Control.Layers(baselayers, overlays);
   map.addControl(layerControl);
 	
